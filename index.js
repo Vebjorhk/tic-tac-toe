ed8eb21da4c42269dac19cb68c9aa3e37d760664
Vebjørn Haug Kåsene <Vebjorhk@users.noreply.github.com>
2024-08-26 21:27:43 +0200

    add player 1, player 2

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
-var gameBoard = document.getElementById("game-board");
+const squares = document.querySelectorAll("[data-custom]");
+var gameGoing = false;
+var player = "1";
 
-document.addEventListener("click", (event) => {
-    if (event.target.classList.contains("square")) {
-        console.log(event.target)
-        event.target.innerHTML = "X"
-    }
-})
+var list = [
+    [0, 0, 0], 
+    [0, 0, 0], 
+    [0, 0, 0]
+]
 
-function place_character(player) {
-    if (player == "1") {
+const rowSums = list.map(row => row.reduce((sum, num) => sum + num, 0));
+console.log(rowSums)
 
-    }
-    else {
+startGame();
 
+
+function startGame() {
+    squares.forEach(square => {
+        square.addEventListener("click", handleSquareClick, {once : true});
+    })
+
+    document.getElementById("default-button").addEventListener("click", buttonClick)
+}
+
+function handleSquareClick(event) {
+    if (gameGoing) {
+        if (player == "1") {
+            event.target.innerHTML = "X";
+            player = "2";
+        }
+        else {
+            event.target.innerHTML = "O";
+            player = "1";
+        }
+        document.getElementById("player-indicator").innerHTML = `Player ${player} is playing`;
     }
 }
+
+
+function buttonClick(event) {
+    gameGoing = !gameGoing;
+    event.target.innerHTML = gameGoing ? "Clear" : "Start Game";
+    if (!gameGoing) {
+        clearBoard()
+    }
+}
+
+
+function clearBoard() {
+    Array.from(squares).forEach((element) => {
+        element.innerHTML = "";
+        element.addEventListener("click", handleSquareClick, {once : true});
+    })
+    player = "1";
+    document.getElementById("player-indicator").innerHTML = `Player ${player} is playing`;
+    gameGoing = true;
+}
+
+
+function checkWinner() {
+    let row_sums = list.map(row => row.reduce((sum, num) => sum + num, 0));
+    
+}
+
